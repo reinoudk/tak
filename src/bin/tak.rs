c4964cc3617e66d796673b0e9b5fc9d3ccd03096
Reinoud Kruithof <2184455+reinoudk@users.noreply.github.com>
2022-01-12 00:47:02 +0100

    refactor: use clap v3
    This switches the argument parsing to use the Derive API.

---
+++ src/bin/tak.rs
@@ -0,0 +1,28 @@
+use clap::Parser;
+
+use tak::cmd::next::{self, NextOpts};
+
+/// tak - a Git tagging helper
+#[derive(Parser)]
+#[clap(version)]
+struct Opts {
+    #[clap(subcommand)]
+    sub_cmd: SubCommand,
+}
+
+#[derive(Parser)]
+enum SubCommand {
+    Next(NextOpts),
+}
+
+fn main() {
+    let opts: Opts = Opts::parse();
+
+    if let Err(err) = match &opts.sub_cmd {
+        SubCommand::Next(next_opts) => {
+            next::exec(next_opts)
+        }
+    } {
+        eprintln!("Error: {}", err.to_string());
+    }
+}
--- src/cmd/next.rs
+++ src/cmd/next.rs
@@ -1,15 +1,10 @@
-use std::convert::TryFrom;
+use clap::{ArgEnum, Parser};
 
-use clap::{App, Arg, ArgMatches, SubCommand};
+use crate::error::Result;
+use crate::git::SemanticRepository;
+use crate::increment::Increment;
 
-use tak::error::Result;
-use tak::git::SemanticRepository;
-use tak::increment::Increment;
-
-pub const CMD_NAME: &'static str = "next";
-pub const INCREMENT_ARG_NAME: &'static str = "increment";
-pub const NO_PREFIX_ARG_NAME: &'static str = "no-prefix";
-
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ArgEnum)]
 enum IncrementArg {
     PATCH,
     MINOR,
@@ -17,53 +12,22 @@ enum IncrementArg {
     AUTO,
 }
 
-impl TryFrom<&str> for IncrementArg {
-    type Error = String;
-
-    fn try_from(value: &str) -> std::result::Result<Self, Self::Error> {
-        match value {
-            "patch" => Ok(IncrementArg::PATCH),
-            "minor" => Ok(IncrementArg::MINOR),
-            "major" => Ok(IncrementArg::MAJOR),
-            "auto" => Ok(IncrementArg::AUTO),
-            _ => Err(String::from(
-                "increment should be one of [patch, minor, major, auto]",
-            )),
-        }
-    }
+/// Determine the next version
+#[derive(Parser, Debug)]
+pub struct NextOpts {
+    /// The type of version increment to use
+    #[clap(arg_enum, default_value_t = IncrementArg::AUTO)]
+    increment: IncrementArg,
+    /// Don't use the 'v' prefix
+    #[clap(long, short)]
+    no_prefix: bool,
 }
 
-pub fn cmd<'a, 'b>() -> App<'a, 'b> {
-    SubCommand::with_name(CMD_NAME)
-        .about("show the next version")
-        .arg(
-            Arg::with_name(INCREMENT_ARG_NAME)
-                .default_value("auto")
-                .validator(validate_increment_arg)
-                .help("major|minor|patch|auto"),
-        )
-        .arg(
-            Arg::with_name(NO_PREFIX_ARG_NAME)
-                .long("no-prefix")
-                .help("disable 'v' prefix of versions"),
-
-        )
-}
-
-fn validate_increment_arg(s: String) -> std::result::Result<(), String> {
-    IncrementArg::try_from(s.as_str()).and(Ok(()))
-}
-
-pub fn exec(sub_matches: &ArgMatches) -> Result<()> {
-    // Unwrapping should always succeed: the increment argument has a default value and is validated
-    let increment_arg = sub_matches.value_of(INCREMENT_ARG_NAME).unwrap();
-    let increment = IncrementArg::try_from(increment_arg).unwrap();
-
-    let prefix = if sub_matches.is_present(NO_PREFIX_ARG_NAME) { "" } else { "v" };
-
+pub fn exec(next: &NextOpts) -> Result<()> {
+    let prefix = if next.no_prefix { "" } else { "v" };
     let repo = SemanticRepository::open_with_prefix(prefix)?;
 
-    let new_version = match increment {
+    let new_version = match next.increment {
         IncrementArg::MAJOR => repo.next_version(Increment::MAJOR),
         IncrementArg::MINOR => repo.next_version(Increment::MINOR),
         IncrementArg::PATCH => repo.next_version(Increment::PATCH),
@@ -72,38 +36,4 @@ pub fn exec(sub_matches: &ArgMatches) -> Result<()> {
 
     println!("{}{}", prefix, new_version?.to_string());
     Ok(())
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    macro_rules! test_validation_ok {
-        ( $name:ident, $s:expr) => {
-            #[test]
-            fn $name() {
-                let arg = String::from($s);
-                let result = validate_increment_arg(arg);
-                assert!(result.is_ok());
-            }
-        };
-    }
-
-    test_validation_ok!(major_works, "major");
-    test_validation_ok!(minor_works, "minor");
-    test_validation_ok!(patch_works, "patch");
-    test_validation_ok!(auto_works, "auto");
-
-    macro_rules! test_validation_fail {
-        ( $name:ident, $s:expr) => {
-            #[test]
-            fn $name() {
-                let arg = String::from($s);
-                let result = validate_increment_arg(arg);
-                assert!(result.is_err());
-            }
-        };
-    }
-
-    test_validation_fail!(bogus_does_not_work, "bogus");
 }
